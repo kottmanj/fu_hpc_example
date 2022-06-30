d9b4ac6fe3b634b37ee821af3bb7b77721191654
J. S. Kottmann <jakob.kottmann@gmail.com>
2022-06-30 14:31:52 +0200

    Update example.py

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import tequila as tq
 
 geometry = "Be 0.0 0 0.0 0.0\n H 0.0 0.0 1.5\n H 0.0 0.0 -1.5"
 # needs pyscf or psi4 installed
-mol = tq.Molecule(geometry=geom, basis_set="sto-3g")
+mol = tq.Molecule(geometry=geometry, basis_set="sto-3g")
 H = mol.make_hamiltonian()
 U = mol.make_ansatz(name="UpCCGSD")
 E = tq.ExpectationValue(H=H, U=U)
